37e3f5034f56bb3bb5b619079bab6f9c162f97f6
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-05 14:25:20 -0800

    Add files via upload

--- pythonclubProject/pythonclubProject/settings.py
+++ pythonclubProject/pythonclubProject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pythonclubProject.wsgi.application'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'techprojectdb',
+        'NAME': 'pythonClubDB',
         'USER' : 'postgres',
         'PASSWORD': 'P@ssw0rd1',
         'HOST' :'localhost',
