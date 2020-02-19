aedc80e07bc5a93f202e4bfb03bd1b8f1945b3a0
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-02-19 13:43:55 -0800

    Assignment 7 update

--- pythonclubProject/pythonclubapp/views.py
+++ pythonclubProject/pythonclubapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import ProductType, Product, Review
 
+
 # Create your views here.
 def index (request):
     return render(request, 'pythonclubapp/index.html')
