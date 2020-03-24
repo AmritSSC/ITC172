ac796322bac3122bbaa7fe03d2557f33323c4353
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2020-03-23 23:41:13 -0700

    Add files via upload

--- final/userDBproj/userDBproj/urls.py
+++ final/userDBproj/userDBproj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userDBapp/', include('userDBapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
